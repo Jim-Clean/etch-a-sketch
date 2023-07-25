b26d5a30cb01ec4391b441c7c7bbad5ce8c45013
Jim-Clean <128581808+Jim-Clean@users.noreply.github.com>
2023-07-25 12:37:21 +0100

    Add clear button onclick event and function

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ function addElement() {
     const newDiv = document.createElement("div");
     const currentDiv = document.getElementById("grid-container");
     currentDiv.appendChild(newDiv);
-    //newDiv.addEventListener('mousedown', touchHandler, false);
     newDiv.addEventListener('mousemove', touchHandler, false);
     }
 
-    //rainbow color mode
+    //radio selects color mode
     function touchHandler(e) {
         if (e.type == "mousemove" && e.which == 1 && document.getElementById("rainbow").checked == true) {
             const randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -20,23 +19,16 @@ function addElement() {
         } else if (e.type == "mousemove" && e.which == 1 && document.getElementById("color-picker").checked == true) {
             this.style.backgroundColor = document.getElementById("c").value;
         }
-    }    
-    //normal color picker functioning
-    /*
-    function touchHandler(e) {
-    if (e.type == "mousemove" && e.which == 1) {
-        this.style.backgroundColor = document.getElementById("c").value; 
     }
+    function clearScreen() {
+        
     }
-    */
 }
-
 console.log(document.getElementById("rainbow").value)
 console.log(addElement());
 
 /*
 TODO:
-- make buttons radio instead of checkbox
 - Make button to clear screen
 - Make slider to select the amount of pixels (divs in grid)
 - House screen in an etch a sketch frame
