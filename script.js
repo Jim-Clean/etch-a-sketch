f0a950b963ee764ea38586870d9bfe010834f9fd
Jim-Clean <128581808+Jim-Clean@users.noreply.github.com>
2023-07-25 17:41:14 +0100

    Improve pen responsiveness

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function addElement() {
     const currentDiv = document.getElementById("grid-container");
     currentDiv.appendChild(newDiv);
     newDiv.addEventListener('mousemove', touchHandler, false);
+    newDiv.addEventListener('mousedown', touchHandler, false);
     }
     
     for (i = 0; i < (x*x); i++) {
@@ -37,12 +38,12 @@ function addElement() {
 
     //radio selects color mode
     function touchHandler(e) {
-        if (e.type == "mousemove" && e.which == 1 && document.getElementById("rainbow").checked == true) {
+        if ((e.type == "mousemove" || e.type == "mousedown") && e.which == 1 && document.getElementById("rainbow").checked == true) {
             const randomColor = Math.floor(Math.random()*16777215).toString(16);
             this.style.backgroundColor = "#" + randomColor; 
-        } else if (e.type == "mousemove" && e.which == 1 && document.getElementById("rubber").checked == true){
+        } else if ((e.type == "mousemove" || e.type == "mousedown") && e.which == 1 && document.getElementById("rubber").checked == true){
             this.style.backgroundColor = 'white';   
-        } else if (e.type == "mousemove" && e.which == 1 && document.getElementById("color-picker").checked == true) {
+        } else if ((e.type == "mousemove" || e.type == "mousedown") && e.which == 1 && document.getElementById("color-picker").checked == true) {
             this.style.backgroundColor = document.getElementById("c").value;
         }
     }
@@ -72,8 +73,6 @@ radiobutton = document.getElementById("color-picker");
 radiobutton.checked = true;
 /*
 TODO:
-- clear old dynamically created divs
-- Make slider to select the amount of pixels (divs in grid)
 - Improve pen by making it also paint on click
 - House screen in an etch a sketch frame
 - Add webpage title/styling to buttons ect.
